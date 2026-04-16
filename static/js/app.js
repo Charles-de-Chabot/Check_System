a2272ca1eae7e474d249cdd3eb621358430313c9
Charles-de-Chabot <chabot0302@gmail.com>
2026-04-16 12:09:12 +0200

    filtre des commandes, html

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
+//VARIABLES
+let isCommandListVisible = true;
+
 document.addEventListener("DOMContentLoaded", function(){
     loadQuickStats();
     setInterval(loadQuickStats, 5000)
@@ -34,10 +37,69 @@ function switchView(view){
         LiveView.style.display = "block";
         btnCommands.classList.remove("active");
         btnLive.classList.add("active");
+        // TODO : Start auto-refresh
     } else {
         commandsView.style.display = "block";
         LiveView.style.display = "none";
         btnCommands.classList.add("active");
         btnLive.classList.remove("active");
+        // TODO : Strop auto-refresh
     }
+}
+
+/**
+ * methode qui permet de filtrer les commandes 
+ * 
+ */
+function filterCommands(category, sourceButton){
+    const buttons = document.querySelectorAll(".filter-btn");
+    const items = document.querySelectorAll(".command-item");
+
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    if(sourceButton){
+        sourceButton.classList.add("active");
+    }
+
+    items.forEach((item) => {
+        if(category === "all" || item.dataset.category === category){
+            item.style.display = "flex";
+        }else{
+            item.style.display = "none";
+        }
+    })
+}
+
+/**
+ * methode qui permet de masquer toutes les cartes de commandes 
+ */
+function toggleCommandList(){
+    const grid = document.getElementById("commandsGrid");
+    const icon = document.getElementById("toggleIcon");
+
+    isCommandListVisible = !isCommandListVisible;
+
+    grid.style.display = isCommandListVisible ? "grid" : "none";
+    icon.textContent = isCommandListVisible ? "🔻" : "🔺"
+}
+
+
+function executeCommand(cmd){
+    saveToHistory(cmd)
+    document.getElementById("cmdInput").value = cmd
+    document.getElementById("commandForm").onsubmit();
+}
+function saveToHistory(cmd){
+
+}
+
+function clearHistory(){
+
+}
+
+function exportResults(){
+
+}
+
+function copyLastResult(){
+
 }
